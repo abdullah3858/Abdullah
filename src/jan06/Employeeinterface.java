26fbf0761eee335b89d4b7f1fc58a32b91294f52
Abdullah Al Mahmud <almahmud448@gmail.com>
2026-01-06 15:40:25 +0600

    jan06

---
+++ src/jan06/Employeeinterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jan06;
+
+import java.util.Map;
+/**
+ *
+ * @author almah
+ */
+ interface Employeeinterface {
+     Map<Integer, Employee> getAllEmployees();
+     
+     
+}
