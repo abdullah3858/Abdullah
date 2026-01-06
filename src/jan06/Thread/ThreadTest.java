8f7e0a694196edb34056e8e84f162e1a1e7e72fc
Abdullah Al Mahmud <almahmud448@gmail.com>
2026-01-06 18:50:56 +0600

    jan06

---
+++ src/jan06/Thread/ThreadTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jan06.Thread;
+
+/**
+ *
+ * @author almah
+ */
+ public class MyThread extends Thread{
+     
+       @Override
+     public void run(){
+         for (int i = 5; i <=10; i++) {
+             System.out.println("Child thread: "+ i);
+         }
+    
+     }
+}
+ 
